06556ad0ac9eb8a14609fe44e7193e2b89f01c26
linaro <linaro@linaro-ubuntu-desktop.(none)>
2015-05-19 18:04:26 -0700

    Worked on Mapping Node.  Trying to get Odometry (and /tf) to get Published.

--- src/Mapping_Node.cpp
+++ src/Mapping_Node.cpp
@@ -4,6 +4,8 @@
 #include <geometry_msgs/Pose2D.h>
 #include <sensor_msgs/LaserScan.h>
 #include <nav_msgs/OccupancyGrid.h>
+#include <tf/transform_broadcaster.h>
+#include <nav_msgs/Odometry.h>
 #include <stdlib.h>
 #include <stdio.h>
 #include <iostream>
@@ -31,11 +33,28 @@ string Mode = "";
 
 //Program Variables
 double dtime = 0.0;
-
+tf::TransformBroadcaster odom_broadcaster;
+double Pose_X;
+double Pose_Y;
+double Pose_Theta;
 void ICARUS_Rover_Pose_Callback(const geometry_msgs::Pose2D::ConstPtr& msg)
 {
-	cout << "Got a Pose" << endl;
-	printf("Got a Pose\r\n");
+	geometry_msgs::Quaternion odom_quat = tf::createQuaternionMsgFromYaw(msg->theta);
+	geometry_msgs::TransformStamped odom_trans;
+	odom_trans.header.stamp = ros::Time::now();
+	odom_trans.header.frame_id = "odom";
+	odom_trans.child_frame_id = "base_link";
+	odom_trans.transform.translation.x = msg->x;
+	odom_trans.transform.translation.y = msg->y;
+	odom_trans.transform.translation.z = 0.0;
+	odom_trans.transform.rotation = odom_quat;
+        odom_broadcaster.sendTransform(odom_trans);
+	Pose_X = msg->x;
+	Pose_Y = msg->y;
+	Pose_Theta = msg->theta;
+	
+
+	printf("Got a Pose x: %f y: %f theta: %f\r\n",msg->x,msg->y,msg->theta);
 }
 void ICARUS_Sonar_Scan_Callback(const sensor_msgs::LaserScan::ConstPtr& msg)
 {
@@ -57,15 +76,17 @@ int main(int argc, char **argv)
   ros::Rate loop_rate(100);
   std::clock_t    start;
   ros::Publisher Pub_ICARUS_OccupancyGrid;  
-  
+  ros::Publisher Pub_ICARUS_Rover_Odom;    
   ros::Subscriber Sub_Rover_Pose;
   ros::Subscriber Sub_Sonar_Scan;
+
  if(Mode.compare("Sim") == 0)
   {
     Pub_ICARUS_OccupancyGrid = nh.advertise<nav_msgs::OccupancyGrid>("ICARUS_SimOccupancyGrid",1000);
-    Sub_Rover_Pose = nh.subscribe<geometry_msgs::Pose2D>("ICARUS_SimRover_Pose",1000,ICARUS_Rover_Pose_Callback);
+    Sub_Rover_Pose = nh.subscribe<geometry_msgs::Pose2D>("/Matlab_Node/ICARUS_SimRover_Pose",1000,ICARUS_Rover_Pose_Callback);
     cout << Sub_Rover_Pose << endl;
-    Sub_Sonar_Scan = nh.subscribe<sensor_msgs::LaserScan>("ICARUS_SimSonar_Scan",1000,ICARUS_Sonar_Scan_Callback);
+    Sub_Sonar_Scan = nh.subscribe<sensor_msgs::LaserScan>("/Matlab_Node/ICARUS_SimSonar_Scan",1000,ICARUS_Sonar_Scan_Callback);
+    Pub_ICARUS_Rover_Odom = nh.advertise<nav_msgs::Odometry>("ICARUS_SimRover_Odom",1000);
     cout << "Sim Mode" << endl;
   }
   else if(Mode.compare("Live") == 0)
@@ -73,6 +94,7 @@ int main(int argc, char **argv)
     Pub_ICARUS_OccupancyGrid = nh.advertise<nav_msgs::OccupancyGrid>("ICARUS_OccupancyGrid",1000);
     Sub_Rover_Pose = nh.subscribe<geometry_msgs::Pose2D>("/Motion_Controller_Node/ICARUS_Rover_Pose",1000,ICARUS_Rover_Pose_Callback);
     Sub_Sonar_Scan = nh.subscribe<sensor_msgs::LaserScan>("/Sonic_Controller_Node/ICARUS_Sonar_Scan",1000,ICARUS_Sonar_Scan_Callback);
+    Pub_ICARUS_Rover_Odom = nh.advertise<nav_msgs::Odometry>("ICARUS_Rover_Odom",1000);
     cout << "Live Mode" << endl;
   }	
  
@@ -99,6 +121,8 @@ int main(int argc, char **argv)
              //ICARUS Diagnostics Publisher
             ICARUS_Diagnostic.header.stamp = ros::Time::now();
             Pub_ICARUS_Mapping_Diagnostic.publish(ICARUS_Diagnostic);
+	    nav_msgs::Odometry Rover_Odometry;
+	    Pub_ICARUS_Rover_Odom.publish(Rover_Odometry);
 	  }
 	  catch(const std::exception& ex)
 	  {
